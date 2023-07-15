148dd46610e3f54b877923ceaf53f0d7e631bf10
SixSiege <mypoptropica2002@gmail.com>
2023-07-15 17:52:36 +0700

    0.9-ish

--- src/main.js
+++ src/main.js
@@ -2,13 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'leaflet/dist/leaflet.css';
 import router from './router/router';
-// import BootstrapVue from 'bootstrap-vue';
 
 import VueSessionStorage from 'vue-sessionstorage';
 Vue.use(VueSessionStorage);
-
-// import "../src/assets/scss/custom.scss";
-// Vue.use(BootstrapVue);
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
